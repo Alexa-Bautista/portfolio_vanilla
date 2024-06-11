f67549215a5fd28c1ef2a8f67ddb7901691727e1
Alexa Bautista <alexa.bautista@genstudents.org>
2024-06-11 00:53:23 -0500

    Create component background

---
+++ components/background.js
@@ -0,0 +1,3 @@
+const backgroundPortfolio = document.getElementById("backgroundPortfolio")
+
+backgroundPortfolio.innerHTML = '<div class="planet1"></div><div class="planet2"></div><div class="planet3"></div><div class="stars"><img src="/public/images/stars-ivnova-portfolio.png" alt="backgroundStars"><img src="/public/images/stars-ivnova-portfolio.png" alt="backgroundStars"><img src="/public/images/stars-ivnova-portfolio.png" alt="backgroundStars"></div>'
